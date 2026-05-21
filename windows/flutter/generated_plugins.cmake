a9371b4259b27c8199709661735d9fcacc3bd27b
Babajide James <teestartaiwo121@gmail.com>
2026-05-21 19:46:28 +0100

    fix: remove duplicate url_launcher entry in pubspec.yaml

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   agora_rtc_engine
+  app_links
   desktop_drop
   flutter_secure_storage_windows
   iris_method_channel
@@ -14,9 +15,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   tray_manager
   url_launcher_windows
   window_manager
-  app_links
-  flutter_secure_storage_windows
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
